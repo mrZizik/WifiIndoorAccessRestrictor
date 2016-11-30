03e828f92c44df5fb7f084402f271937cccfec7a
mrZizik <mrfreezzzer@mail.ru>
2016-11-30 18:35:52 +0300

    all frames fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ ap_list = []
 def PacketHandler(pkt):
     if pkt.haslayer(Dot11):
         if pkt.addr2 not in ap_list:
-            print "SRC MAC: {0} DST MAC: {1}".format(pkt.addr, pkt.addr2)
+            print "SRC MAC: {0} DST MAC: {1}".format(pkt.addr1, pkt.addr2)
 
 
 sniff(iface="mon0", prn = PacketHandler)
