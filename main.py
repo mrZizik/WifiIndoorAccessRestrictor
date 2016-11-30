c4e716b91f1bdb3192191cd1ba4117dd913ca81b
mrZizik <mrfreezzzer@mail.ru>
2016-11-30 20:01:35 +0300

    fix

--- main.py
+++ main.py
@@ -2,14 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from scapy.all import *
-
-ap_list = []
+blacklists = open("black.txt","r").read().split("\n")
 
 def PacketHandler(pkt):
     if pkt.haslayer(Dot11):
-        if pkt.addr2 not in ap_list:
-            ap_list.append(pkt.addr2)
-            print "SENDER MAC: {0} RECIPIEN SSID: {1}".format(pkt.addr2, pkt.addr1)
+        if pkt.addr2 in blacklists:
+            print "Notify breach - {0}".format(pkt.addr2)
 
 
 sniff(iface="mon0", prn = PacketHandler)
