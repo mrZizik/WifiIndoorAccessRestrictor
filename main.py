f53652512bd41082272411c2faff0e9e12daa27d
mrZizik <mrfreezzzer@mail.ru>
2016-11-30 22:02:47 +0300

    fix

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from time import gmtime, strftime
 from scapy.all import *
 blacklist = open("black.txt","r").read().split("\n")
 logfile = open("logs.txt","a+")
-
+breaks=[]
 def notify(addr):
-    time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    logfile.write("[{0}] - {1} from blacklist is in secured area".format(time, addr))
-    print "[{0}] - {1} from blacklist is in secured area".format(time, addr)
-
+    if addr not in breaks:
+        time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        logfile.write("[{0}] - {1} from blacklist is in secured area\n".format(time, addr))
+        print "[{0}] - {1} from blacklist is in secured area".format(time, addr)
+        breaks.append(addr)
 def PacketHandler(pkt):
     if pkt.addr2 in blacklist:
         notify(pkt.addr2)
