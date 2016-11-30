9ccdbdf99f229ab7590ea46d59f445f0db30b97d
mrZizik <mrfreezzzer@mail.ru>
2016-11-30 18:33:47 +0300

    all frames fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ ap_list = []
 def PacketHandler(pkt):
     if pkt.haslayer(Dot11):
         if pkt.addr2 not in ap_list:
-            print "AP MAC: %s with SSID: %s " %(pkt.addr2, pkt.info)
+            print "AP MAC: %s with SSID " %(pkt.addr2)
 
 
 sniff(iface="mon0", prn = PacketHandler)
