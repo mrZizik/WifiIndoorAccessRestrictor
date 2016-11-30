51b0731fac3abb29d2c507efa22bffe12587312b
mrZizik <mrfreezzzer@mail.ru>
2016-11-30 19:05:10 +0300

    fix

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ ap_list = []
 def PacketHandler(pkt):
     if pkt.haslayer(Dot11):
         if pkt.addr2 not in ap_list:
-            print "SRC MAC: {0} DST MAC: {1}".format(dir(pkt), pkt.addr2)
+            ap_list.append(pkt.addr2)
+            print "AP MAC: {0} with SSID:".format(pkt.summary())
 
 
 sniff(iface="mon0", prn = PacketHandler)
