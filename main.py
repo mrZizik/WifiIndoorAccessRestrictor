5528e153d65ad4b5e10b654e0b2dc8667732a07d
mrZizik <mrfreezzzer@mail.ru>
2016-11-30 21:03:39 +0300

    fix

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from time import gmtime, strftime
 from scapy.all import *
-blacklists = open("black.txt","r").read().split("\n")
+blacklist = open("black.txt","r").read().split("\n")
+logfile = open("logs.txt","a+")
+
+def notify(addr):
+    time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    logfile.write("[{0}] - {1} from blacklist is in secured area".format(time, addr))
+    print "[{0}] - {1} from blacklist is in secured area".format(time, addr)
 
 def PacketHandler(pkt):
     if pkt.haslayer(Dot11):
-        if pkt.addr2 in blacklists or pkt.addr1 in blacklists:
-            print "Notify breach "
+        if pkt.addr2 in blacklist:
+            notify(pkt.addr2)
+        elif pkt.addr in blacklist:
+            notify(pkt.addr)
 
 
 sniff(iface="mon0", prn = PacketHandler)
