4531b0217f78b5e06f99d51967bed568817a2a11
root <mrfreezzzer@mail.ru>
2016-12-04 22:19:22 +0300

    fix

--- main.py
+++ main.py
@@ -1,40 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+from time import gmtime, strftime
 from scapy.all import *
 import os
 import time
 import thread
-import ur
-import sched, time
+import urllib3
+http = urllib3.PoolManager()
 
-blacklist = []
-
-def timerFunc(sc): 
-    print "Updating white list"
-    blacklist = urllib.urlopen("http://dagmeet.appspot.com/LIST")
-    s.enter(60, 1, timerFunc, (sc,))
-
-
-
+## blacklist = open("black.txt","r".read().split("\n")
 logfile = open("logs.txt","a+")
 breaks=[]
 
 
 def request(str):
-	data = {}
-	data["message"] = str
-	url_values = urllib.urlencode(data)
-	url = "http://dagmeet.appspot.com/NOTIFY"
-	urllib.urlopen(url + "?" + url_values)
-
-def getList()
+	http.request("GET", "http://dagmeet.appspot.com/NOTIFY", fields={"mac": str})
+	
 
 
 def notify(addr):
     if addr not in breaks:
         time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        logfile.write("[{0}] - {1} from blacklist is in secured area\n".format(time, addr))
+        logfile.write("[{0}] - {1} not in whitelist is in secured area\n".format(time, addr))
         print "[{0}] - {1} from blacklist is in secured area".format(time, addr)
         request("[{0}] - {1} from blacklist is in secured area".format(time, addr))
         breaks.append(addr)
@@ -42,15 +31,9 @@ def notify(addr):
 def PacketHandler(pkt):
     if pkt.addr2 in blacklist:
         notify(pkt.addr2)
-    elif pkt.addr1 in blacklist:
-        notify(pkt.addr1)
+    # elif pkt.addr1 in blacklist:
+    #     notify(pkt.addr1)
 
 
-s = sched.scheduler(time.time, time.sleep)
-s.enter(60, 1, do_something, (s,))
-s.run()
 
 sniff(iface="mon0", prn = PacketHandler)
-
-
-
